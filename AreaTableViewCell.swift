2dc5d92ee7a8716682a3751a90089e35d4ede494
Jon St John <jonstjohn@gmail.com>
2016-04-30 12:55:20 -0600

    Started adding area table view controller and cells

---
+++ AreaTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  AreaTableViewCell.swift
+//  climbingweather
+//
+//  Created by Jon St John on 4/29/16.
+//
+//
+
+import UIKit
+
+class AreaTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var name: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ NearbyTableViewController.swift
@@ -0,0 +1,108 @@
+//
+//  NearbyTableViewController.swift
+//  climbingweather
+//
+//  Created by Jon St John on 4/29/16.
+//
+//
+
+import UIKit
+
+struct ClimbingArea {
+    var name: String?
+}
+
+struct ClimbingAreas {
+    var areas = [ClimbingArea]()
+}
+
+class NearbyTableViewController: UITableViewController {
+    
+    var areas = [ClimbingArea]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.areas.append(ClimbingArea(name: "New River Gorge"))
+        
+        self.tableView.registerNib(UINib(nibName: "AreaTableViewCell", bundle: nil), forCellReuseIdentifier: "AreaCell")
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.areas.count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("AreaCell", forIndexPath: indexPath) as! AreaTableViewCell
+
+        let area = areas[indexPath.row]
+        if let name = area.name {
+            cell.name.text = name
+        }
+
+        return cell
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
