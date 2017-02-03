9b3d62c24f9242585353ffe85677d611fb230384
Jon St John <jstjohn@anonyome.com>
2017-02-02 21:59:29 -0700

    Initial swift

---
+++ Classes/Area.swift
@@ -0,0 +1,15 @@
+//
+//  Area.swift
+//  climbingweather
+//
+//  Created by Jon St. John on 2/2/17.
+//
+//
+
+import Foundation
+
+struct Area {
+    var areaId: String
+    var name: String
+    var favorite: Bool?
+}
