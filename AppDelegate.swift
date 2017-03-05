71b5b36155fa5a791668b4ddcf45787f3788ac80
Jon St John <jstjohn@anonyome.com>
2017-03-04 21:58:46 -0700

    Remove objective-c hourly cell

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 //
 import Foundation
+import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, UITabBarControllerDelegate {
--- Classes/Forecast.swift
+++ Classes/Forecast.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 
 struct Precipitation {
     let rain: Decimal?
