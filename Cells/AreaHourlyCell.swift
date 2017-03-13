07befd6ef9a9d221f7178ceb853c3800e190cc0a
Jon St John <jstjohn@anonyome.com>
2017-03-13 15:41:48 -0600

    Add area hourly cell

---
+++ Cells/AreaHourlyCell.swift
@@ -0,0 +1,24 @@
+//
+//  AreaHourlyCell.swift
+//  climbingweather
+//
+//  Created by Jon St. John on 3/13/17.
+//
+//
+
+import UIKit
+
+class AreaHourlyCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
