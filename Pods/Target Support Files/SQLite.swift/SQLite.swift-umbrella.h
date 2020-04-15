2d41f69b619f498aa4d57d2b66d837446bee3fc8
Jon St John <jstjohn@anonyome.com>
2020-04-15 09:15:20 -0600

    Update fastlane certs and fix project settings

--- Pods/Target Support Files/SQLite.swift/SQLite.swift-umbrella.h
+++ Pods/Target Support Files/SQLite.swift/SQLite.swift-umbrella.h
@@ -12,7 +12,6 @@
 
 #import "SQLite.h"
 #import "SQLiteObjc.h"
-#import "SQLite-Bridging.h"
 
 FOUNDATION_EXPORT double SQLiteVersionNumber;
 FOUNDATION_EXPORT const unsigned char SQLiteVersionString[];
